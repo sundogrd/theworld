26ae1066ab9e4c48cdcd4cabb0b899099a575540
lwio <liang.pearce@gmail.com>
2019-10-04 15:27:20 +0800

    feat: init world

--- packages/theworld-core/src/World.ts
+++ packages/theworld-core/src/World.ts
@@ -2,8 +2,16 @@ import { EventEmitter } from "events";
 
 // the class take responsibility for everything the player do and display
 class World extends EventEmitter {
-    constructor(worldData: any) {
+    constructor(worldDir: any) {
         super()
+    }
+
+    run(): void {
+        throw new Error("not implemented")
+    }
+
+    load(): void {
+        throw new Error("not implemented")
     }
 }
 
