b2b5ed7eb8990a3f238de82936b9a5b4f6c87175
lwio <liang.pearce@gmail.com>
2019-10-07 10:57:08 +0800

    feat: add bundle

---
+++ packages/fantasy-base-bundle/creatures/creatures/lwio/index.js
@@ -0,0 +1,42 @@
+const Bundle = require('theworld-core').Bundle;
+const keyboard = Bundle.createItem(Bundle.getItemTemplate('keyboard'));
+const macbook = Bundle.createItem(Bundle.getItemTemplate('macbook'));
+
+module.exports = {
+    id: 'lwio', // one of the author's nickname
+    name: '${lwio}',
+    description: 'one of the founder of theworld and sundog',
+    gender: 'male',
+    race: 'human',
+    inventory: {
+        maxItem: 50,
+        items: {
+            [macbook.id]: macbook,
+        },
+    },
+    equipment: {
+        'right-hand': keyboard, // using keyboard to hit :)
+    },
+    templateId: null,
+    state: 'thinking', // 保留，智能状态机 state machine
+    position: {
+        areaId: 'zhongweitong',
+        x: 0,
+        y: 0,
+        direction: 'south',
+    },
+    attributes: {
+        'magic-power': 400,
+    },
+    isAlive: true, // false if the creature is dead. :)
+    think: (world, player, me) => {
+        if (me.isAlive) {
+            return {
+                actionId: 'think',
+            };
+        }
+    },
+    meta: {
+        isGod: true,
+    },
+};
