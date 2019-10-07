127da720958acd2984d8de869c32a227795f72bd
lwio <liang.pearce@gmail.com>
2019-10-07 12:04:05 +0800

    chore: format

--- packages/fantasy-base-bundle/index.js
+++ packages/fantasy-base-bundle/index.js
@@ -1,68 +1,113 @@
-const Bundle = require("theworld-core").Bundle
-const path = require("path")
-const fse = require("fs-extra")
+const Bundle = require('theworld-core').Bundle;
+const path = require('path');
+const fse = require('fs-extra');
 
-async function regitserI18n() {
-    const i18nDirectories = fse.readdirSync(path.resolve("./i18n"), { withFileTypes: true }).filter(dirent => dirent.isDirectory())
+async function registerI18n() {
+    const i18nDirectories = fse
+        .readdirSync(path.resolve('./i18n'), { withFileTypes: true })
+        .filter(dirent => dirent.isDirectory());
 
-    await Promise.all(i18nDirectories.map(async dir => {
-        await Bundle.registerI18n(dir.name, require(path.resolve("./i18n", dir.name, "index.js")))
-    }))
+    await Promise.all(
+        i18nDirectories.map(async dir => {
+            await Bundle.registerI18n(
+                dir.name,
+                require(path.resolve('./i18n', dir.name, 'index.js')),
+            );
+        }),
+    );
 }
 
-function registerItems() {
+async function registerItems() {
     // register items
-    const itemDirectories = fse.readdirSync(path.resolve("./items/items"), { withFileTypes: true }).filter(dirent => dirent.isDirectory())
+    const itemDirectories = fse
+        .readdirSync(path.resolve('./items/items'), { withFileTypes: true })
+        .filter(dirent => dirent.isDirectory());
 
-    await Promise.all(itemDirectories.map(async dir => {
-        await Bundle.registerItem(require(path.resolve("./items/items", dir.name, "index.js")))
-    }))
+    await Promise.all(
+        itemDirectories.map(async dir => {
+            await Bundle.registerItem(
+                require(path.resolve('./items/items', dir.name, 'index.js')),
+            );
+        }),
+    );
 
     // register template
-    const templateFiles = fse.readdirSync(path.resolve("./items/templates"), { withFileTypes: true }).filter(dirent => !dirent.isDirectory())
-    await Promise.all(templateFiles.map(async file => {
-        await Bundle.registerItemTemplate(require(path.resolve("./items/templates", dir.name)))
-    }))
+    const templateFiles = fse
+        .readdirSync(path.resolve('./items/templates'), { withFileTypes: true })
+        .filter(dirent => !dirent.isDirectory());
+    await Promise.all(
+        templateFiles.map(async file => {
+            await Bundle.registerItemTemplate(
+                require(path.resolve('./items/templates', dir.name)),
+            );
+        }),
+    );
 }
 
-function registerCreatures() {
+async function registerCreatures() {
     // register items
-    const itemDirectories = fse.readdirSync(path.resolve("./items/items"), { withFileTypes: true }).filter(dirent => dirent.isDirectory())
+    const itemDirectories = fse
+        .readdirSync(path.resolve('./items/items'), { withFileTypes: true })
+        .filter(dirent => dirent.isDirectory());
 
-    await Promise.all(itemDirectories.map(async dir => {
-        await Bundle.registerItem(require(path.resolve("./items/items", dir.name, "index.js")))
-    }))
+    await Promise.all(
+        itemDirectories.map(async dir => {
+            await Bundle.registerItem(
+                require(path.resolve('./items/items', dir.name, 'index.js')),
+            );
+        }),
+    );
 
     // register template
-    const templateFiles = fse.readdirSync(path.resolve("./items/templates"), { withFileTypes: true }).filter(dirent => !dirent.isDirectory())
-    await Promise.all(templateFiles.map(async file => {
-        await Bundle.registerItemTemplate(require(path.resolve("./items/templates", file.name)))
-    }))
+    const templateFiles = fse
+        .readdirSync(path.resolve('./items/templates'), { withFileTypes: true })
+        .filter(dirent => !dirent.isDirectory());
+    await Promise.all(
+        templateFiles.map(async file => {
+            await Bundle.registerItemTemplate(
+                require(path.resolve('./items/templates', file.name)),
+            );
+        }),
+    );
 }
 
-function registerAttributes() {
+async function registerAttributes() {
     // register items
-    const attributesDirectories = fse.readdirSync(path.resolve("./attributes"), { withFileTypes: true }).filter(dirent => dirent.isDirectory())
+    const attributesDirectories = fse
+        .readdirSync(path.resolve('./attributes'), { withFileTypes: true })
+        .filter(dirent => dirent.isDirectory());
 
-    await Promise.all(attributesDirectories.map(async dir => {
-        await Bundle.registerItem(dir.name, require(path.resolve("./attributes", dir.name, "index.js")))
-    }))
+    await Promise.all(
+        attributesDirectories.map(async dir => {
+            await Bundle.registerItem(
+                dir.name,
+                require(path.resolve('./attributes', dir.name, 'index.js')),
+            );
+        }),
+    );
 }
 
-function registerActions() {
-    const actionDirectories = fse.readdirSync(path.resolve("./actions"), { withFileTypes: true }).filter(dirent => dirent.isDirectory())
+async function registerActions() {
+    const actionDirectories = fse
+        .readdirSync(path.resolve('./actions'), { withFileTypes: true })
+        .filter(dirent => dirent.isDirectory());
 
-    await Promise.all(actionDirectories.map(async dir => {
-        await Bundle.registerItem(dir.name, require(path.resolve("./actions", dir.name, "index.js")))
-    }))
+    await Promise.all(
+        actionDirectories.map(async dir => {
+            await Bundle.registerItem(
+                dir.name,
+                require(path.resolve('./actions', dir.name, 'index.js')),
+            );
+        }),
+    );
 }
 
 module.exports = {
     init: () => {
-        registerI18n()
-        registerActions()
-        registerAttributes()
-        registerItems()
-        registerCreatures()
-    }
-}
+        registerI18n();
+        registerActions();
+        registerAttributes();
+        registerItems();
+        registerCreatures();
+    },
+};
