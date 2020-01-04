6397e0cfb276ab4e325a80f1a6d5a1b63ef6f8fb
lwio <liang.pearce@gmail.com>
2020-01-04 18:06:07 +0800

    feat: pause

--- packages/theworld-ui/src/renderer/views/GameView/actions/index.tsx
+++ packages/theworld-ui/src/renderer/views/GameView/actions/index.tsx
@@ -7,28 +7,48 @@ import "react-grid-layout/css/styles.css"
 import './index.less';
 
 // for test
-const actions = {
+const actions: Record<string, Action> = {
     "move-north": {
         id: "move-north",
         name: "向北走",
-        short: "w",
+        shortcut: "w",
+    },
+    "move-south": {
+        id: "move-south",
+        name: "向南走",
+        shortcut: "s",
+    },
+    "move-west": {
+        id: "move-west",
+        name: "向西走",
+        shortcut: "a",
+    },
+    "move-east": {
+        id: "move-east",
+        name: "向东走",
+        shortcut: "d",
     }
 }
 
+const layout = [
+    {i: 'move-north', x: 1, y: 0, w: 1, h: 1},
+    {i: 'move-south', x: 1, y: 1, w: 1, h: 1},
+    {i: 'move-west', x: 0, y: 0, w: 1, h: 2},
+    {i: 'move-east', x: 2, y: 0, w: 1, h: 2}
+];
+
 const Actions = observer(() => {
-    const layout = [
-        {i: 'w', x: 1, y: 0, w: 1, h: 1},
-        {i: 's', x: 1, y: 1, w: 1, h: 1},
-        {i: 'a', x: 0, y: 0, w: 1, h: 2},
-        {i: 'd', x: 2, y: 0, w: 1, h: 2}
-    ];
+
     return (
         <div className="actions">
             <GridLayout className="layout" layout={layout} cols={12} rowHeight={30} width={1200}>
-                <div className={"action"} key="w">上</div>
-                <div className={"action"} key="s">下</div>
-                <div className={"action"} key="a">左</div>
-                <div className={"action"} key="d">右</div>
+                {
+                    Object.keys(actions).map(actionId => {
+                        return (
+                            <div className={"action"} key={actionId}>{actions[actionId].name}</div>
+                        )
+                    })
+                }
             </GridLayout>
         </div>
     );
