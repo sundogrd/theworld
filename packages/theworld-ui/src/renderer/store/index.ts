0ee714d8e36cee111876cb5c429d110fac916e8c
lwio <liang.pearce@gmail.com>
2020-01-04 17:12:34 +0800

    feat: update

--- packages/theworld-ui/src/renderer/store/index.ts
+++ packages/theworld-ui/src/renderer/store/index.ts
@@ -1,15 +1,12 @@
 import gameStore from './gameStore';
-import routerStore from './routerStore';
 
 export type TStore = {
     gameStore: ReturnType<typeof gameStore>;
-    routerStore: ReturnType<typeof routerStore>;
 }
 
 export function createStore(): TStore {
     // note the use of this which refers to observable instance of the store
     return {
         gameStore: gameStore(),
-        routerStore: routerStore(),
     }
 }
